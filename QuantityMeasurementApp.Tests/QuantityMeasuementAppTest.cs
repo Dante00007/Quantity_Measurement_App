c329306cf1e560451e52e1fefa80bfe144749601
Krishna Sharma <ks1658759@gmail.com>
2026-02-23 15:47:25 +0530

    UC-2 added

--- QuantityMeasurementApp.Tests/QuantityMeasuementAppTest.cs
+++ QuantityMeasurementApp.Tests/QuantityMeasuementAppTest.cs
@@ -8,7 +8,7 @@ public sealed class QuantityMeasuementAppTest
 {
    
     [TestMethod]
-    public void TestEquality_SameValue()
+    public void TestFeetEquality_SameValue()
     {
         
         var feet1 = new QuantityMeasurementApplication.Feet(1.0);
@@ -18,7 +18,7 @@ public sealed class QuantityMeasuementAppTest
     }
 
     [TestMethod]
-    public void TestEquality_DifferentValue()
+    public void TestFeetEquality_DifferentValue()
     {
         var feet1 = new QuantityMeasurementApplication.Feet(1.0);
         var feet2 = new QuantityMeasurementApplication.Feet(2.0);
@@ -27,7 +27,7 @@ public sealed class QuantityMeasuementAppTest
     }
 
     [TestMethod]
-    public void TestEquality_NullComparison()
+    public void TestFeetEquality_NullComparison()
     {
         var feet1 = new QuantityMeasurementApplication.Feet(1.0);
         var feet2 = null as QuantityMeasurementApplication.Feet;
@@ -36,16 +36,59 @@ public sealed class QuantityMeasuementAppTest
     }
 
     [TestMethod]
-    public void TestEquality_NonNumericInput()
+    public void TestFeetEquality_NonNumericInput()
     {
         var feet1 = new QuantityMeasurementApplication.Feet(1.0);
         Assert.IsFalse(feet1.Equals("abc"));
     }
 
     [TestMethod]
-    public void TestEquality_SameReference()
+    public void TestFeetEquality_SameReference()
     {
         var feet1 = new QuantityMeasurementApplication.Feet(1.0);
+        
+        Assert.IsTrue(feet1.Equals(feet1));
+    }
+
+    [TestMethod]
+    public void TestInchEquality_SameValue()
+    {
+        
+        var feet1 = new QuantityMeasurementApplication.Inch(1.0);
+        var feet2 = new QuantityMeasurementApplication.Inch(1.0);
+
+        Assert.IsTrue(feet1.Equals(feet2));
+    }
+
+    [TestMethod]
+    public void TestInchEquality_DifferentValue()
+    {
+        var feet1 = new QuantityMeasurementApplication.Inch(1.0);
+        var feet2 = new QuantityMeasurementApplication.Inch(2.0);
+
+        Assert.IsFalse(feet1.Equals(feet2));
+    }
+
+    [TestMethod]
+    public void TestInchEquality_NullComparison()
+    {
+        var feet1 = new QuantityMeasurementApplication.Inch(1.0);
+        var feet2 = null as QuantityMeasurementApplication.Inch;
+
+        Assert.IsFalse(feet1.Equals(null));
+    }
+
+    [TestMethod]
+    public void TestInchEquality_NonNumericInput()
+    {
+        var feet1 = new QuantityMeasurementApplication.Inch(1.0);
+        Assert.IsFalse(feet1.Equals("abc"));
+    }
+
+    [TestMethod]
+    public void TestInchEquality_SameReference()
+    {
+        var feet1 = new QuantityMeasurementApplication.Inch(1.0);
         
         Assert.IsTrue(feet1.Equals(feet1));
     }
--- QuantityMeasurementApp/QuantityMeasurementApplication.cs
+++ QuantityMeasurementApp/QuantityMeasurementApplication.cs
@@ -31,11 +31,48 @@ public class QuantityMeasurementApplication
         }
     }
 
+    public class Inch
+    {
+        public readonly double _value;
+        public Inch(double value)
+        {
+            this._value = value;
+        }
+        public double Value
+        {
+            get { return _value; }
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+        
+            Inch other = (Inch)obj;
+ 
+            return _value.CompareTo(other._value) == 0;
+        }
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+    }
     public static void DemonstrateFeetEquality()
     {
         Feet feet1 = new Feet(10);
         Feet feet2 = new Feet(10);
         Console.WriteLine(feet1.Equals(feet2));
+    }
+
+    public static void DemonstrateInchEquality()
+    {
+        Inch inch1 = new Inch(10);
+        Inch inch2 = new Inch(10);
+        Console.WriteLine(inch1.Equals(inch2));
     }
     
 }
