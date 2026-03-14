d9d25f1f9a8b21eec2e98a961198f0ea0cbea952
Krishna Sharma <ks1658759@gmail.com>
2026-03-14 11:28:12 +0530

    sdh

--- QuantityMeasurementApp/obj/Debug/net10.0/QuantityMeasurementApp.AssemblyInfo.cs
+++ QuantityMeasurementApp/obj/Debug/net10.0/QuantityMeasurementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuantityMeasurementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8400163bf6e0fd8fb8a8ea8c3bc6eeb22f6471")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd319121151ded85d32c9fdb0bd6cab95170845")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuantityMeasurementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuantityMeasurementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
